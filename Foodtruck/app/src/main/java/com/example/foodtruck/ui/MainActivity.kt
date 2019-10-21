45fb5ba929d73d97568d4e50b43fcf901a0f0f92
vdtyson <versilistyson@gmail.com>
2019-10-21 14:44:29 -0400

    Adds inital app for Foodie Truck

---
+++ Foodtruck/app/src/main/java/com/example/foodtruck/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.foodtruck.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodtruck.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
