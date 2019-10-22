11967503a66b7053f8a437f925c5cc1485b91580
vdtyson <versilistyson@gmail.com>
2019-10-22 14:03:44 -0400

    - Adds get account type

--- Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/NewUser.kt
+++ Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/NewUser.kt
@@ -3,8 +3,15 @@ package com.example.foodtruck.data.source.local.model
 import java.io.Serializable
 
 class NewUser(
-    username: String,
-    password: String,
-    email: String,
-    accountType: AccountType
-) : Serializable
+    val username: String,
+    val password: String,
+    val email: String,
+    val accountType: AccountType
+) : Serializable {
+    fun getAccountType() : String {
+        return when(accountType) {
+            is AccountType.Foodie -> "USER_ACCOUNT"
+            is AccountType.Vendor -> "VENDOR_ACCOUNT"
+        }
+    }
+}
