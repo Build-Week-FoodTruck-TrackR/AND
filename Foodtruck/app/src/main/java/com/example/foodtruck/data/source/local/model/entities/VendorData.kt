bef62603c6e4d944df99fdcf693c0dc990a3d272
vdtyson <versilistyson@gmail.com>
2019-10-22 15:43:29 -0400

    - Merges master into branch

--- Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/entities/VendorData.kt
+++ Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/entities/VendorData.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-
+//
 @Entity(
     tableName = "vendor_data",
     foreignKeys = (
