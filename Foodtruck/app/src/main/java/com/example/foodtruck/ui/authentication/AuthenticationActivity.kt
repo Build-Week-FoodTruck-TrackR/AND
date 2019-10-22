b2e1d65a0c833e858a23aeffa5fba53ab3d185a9
vdtyson <versilistyson@gmail.com>
2019-10-21 23:13:02 -0400

    - Adds functionality for nav controller

--- Foodtruck/app/src/main/java/com/example/foodtruck/ui/authentication/AuthenticationActivity.kt
+++ Foodtruck/app/src/main/java/com/example/foodtruck/ui/authentication/AuthenticationActivity.kt
@@ -2,15 +2,26 @@ package com.example.foodtruck.ui.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.replace
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.foodtruck.R
+import com.example.foodtruck.ui.authentication.fragments.SplashScreenFragment
+import kotlinx.android.synthetic.main.activity_authentication.*
 
 class AuthenticationActivity : AppCompatActivity() {
 
-    private val navController by lazy {
-        
+    private val host by lazy {
+        NavHostFragment.create(R.navigation.auth_nav_graph)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.auth_nav_host_fragment_container, host)
+            .setPrimaryNavigationFragment(host).commit()
     }
 }
