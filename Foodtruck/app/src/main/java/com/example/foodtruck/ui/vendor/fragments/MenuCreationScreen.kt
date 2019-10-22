b55c6ce9c089bba110304a09fcaf8446d9bf7c69
GIANCARLO GATTI <gattigcg1@gmail.com>
2019-10-23 03:30:45 +0800

    worked on dialog more

---
+++ Foodtruck/app/src/main/java/com/example/foodtruck/ui/vendor/fragments/MenuCreationScreen.kt
@@ -0,0 +1,27 @@
+package com.example.foodtruck.ui.vendor.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.foodtruck.R
+
+class MenuCreationScreen: DialogFragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setStyle(STYLE_NORMAL, R.style.AppTheme_FullScreenDialog)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fullscreen_dialog_menu_creation, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
