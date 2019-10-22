823208c1bd96f60a55364fe84e532a32b94df7f1
vdtyson <versilistyson@gmail.com>
2019-10-22 15:20:29 -0400

    - Adds one to many relationship from generic user class to FoodieData and VendorData model.

--- Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/User.kt
+++ Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/User.kt
@@ -1,11 +1,14 @@
 package com.example.foodtruck.data.source.local.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.foodtruck.ui.foodie.fragments.Favorites
 
-@Entity
+@Entity(tableName = "users")
 data class User(
-    @PrimaryKey
-    val id: Int,
-    val username: String
+    @PrimaryKey(autoGenerate = false) @ColumnInfo(name = "user_id")
+    val userId: Int,
+    val username: String,
+    val accountType: AccountType
 )
