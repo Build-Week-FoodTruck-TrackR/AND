4389139b1de8fd6f08b3d196156d2689cbd582a9
GIANCARLO GATTI <gattigcg1@gmail.com>
2019-10-24 02:24:17 +0800

    added spinner menus

--- Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/Foodtruck.kt
+++ Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/Foodtruck.kt
@@ -1,3 +1,16 @@
 package com.example.foodtruck.data.source.local.model
 
-class Foodtruck(val name: String, val model: String, val menu: Menu? = null, val location: String)
+
+//this is the foodtruck we will post to the backend, does not have any current reviews
+open class Foodtruck(val name: String, val model: String, val latitude: Double, val longitude: Double, val menu: Menu? = null)
+
+//when we use a GET request to see the food truck from the backend, we will get all the reviews other diners will have left it.
+class SaveableFoodtruck(val allReviews: MutableList<Reviews>, name: String, model: String, latitude: Double, longitude: Double, menu: Menu? = null) : Foodtruck(name, model, latitude, longitude, menu)
+
+//this is a list of reviews that we are getting back when we query the foodtruck
+class Reviews(val score: Double, val description: String, val userName: String)
+
+
+
+
+
