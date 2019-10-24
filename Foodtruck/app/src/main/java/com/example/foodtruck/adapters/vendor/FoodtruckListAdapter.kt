89e8c2d3ad42560eaacd1680405594e23fc1a42d
GIANCARLO GATTI <gattigcg1@gmail.com>
2019-10-24 22:52:06 +0800

    editing foodtrucks works!

--- Foodtruck/app/src/main/java/com/example/foodtruck/adapters/vendor/FoodtruckListAdapter.kt
+++ Foodtruck/app/src/main/java/com/example/foodtruck/adapters/vendor/FoodtruckListAdapter.kt
@@ -1,20 +1,24 @@
 package com.example.foodtruck.adapters.vendor
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodtruck.R
 import com.example.foodtruck.data.source.local.model.Foodtruck
+import com.example.foodtruck.ui.vendor.fragments.FoodtruckCreationScreen
+import com.example.foodtruck.ui.vendor.fragments.FoodtruckScreen
 import com.example.foodtruck.util.createAlert
 import kotlinx.android.synthetic.main.foodtruck_item_layout.view.*
 
-class FoodtruckListAdapter(val data: MutableList<Foodtruck>): RecyclerView.Adapter<FoodtruckListAdapter.ViewHolder>() {
+class FoodtruckListAdapter(val data: MutableList<Foodtruck>, val foodtruckScreen: FoodtruckScreen): RecyclerView.Adapter<FoodtruckListAdapter.ViewHolder>() {
 
     class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
         val foodTruckName = view.tv_foodtruck_name
         val foodTruckModel = view.tv_foodtruck_model
         val cancelButton = view.img_cancel_btn
+        val editButton = view.img_edit_btn
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -35,6 +39,16 @@ class FoodtruckListAdapter(val data: MutableList<Foodtruck>): RecyclerView.Adapt
             {d, i -> d.dismiss()
                      data.removeAt(position)
                      this.notifyItemRemoved(position) }, {d, i-> }).show()
+        }
+
+        holder.editButton.setOnClickListener {
+            val f = FoodtruckCreationScreen()
+            f.listener = foodtruckScreen
+            val bundle = Bundle()
+            bundle.putSerializable("foodTruckToEdit", data[position])
+            bundle.putInt("foodTruckToEditPosition", position)
+            f.arguments = bundle
+            f.show(foodtruckScreen.fragmentManager!!, "editingFoodTruck")
         }
     }
 }
--- Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/Foodtruck.kt
+++ Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/Foodtruck.kt
@@ -1,8 +1,10 @@
 package com.example.foodtruck.data.source.local.model
 
+import java.io.Serializable
+
 
 //this is the foodtruck we will post to the backend, does not have any current reviews
-open class Foodtruck(val name: String, val model: String, val latitude: Double, val longitude: Double, var menu: Menu? = null)
+open class Foodtruck(val name: String, val model: String, val latitude: Double, val longitude: Double, var menu: Menu? = null) : Serializable
 
 //when we use a GET request to see the food truck from the backend, we will get all the reviews other diners will have left it.
 class SaveableFoodtruck(val allReviews: MutableList<Reviews>, name: String, model: String, latitude: Double, longitude: Double, menu: Menu? = null) : Foodtruck(name, model, latitude, longitude, menu)
