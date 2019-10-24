f5fa8437d651c25e63ab5ef535f4ff3c94278b40
GIANCARLO GATTI <gattigcg1@gmail.com>
2019-10-25 00:13:19 +0800

    menu viewing in vendor works, working on favorites fragment

--- Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/Foodtruck.kt
+++ Foodtruck/app/src/main/java/com/example/foodtruck/data/source/local/model/Foodtruck.kt
@@ -7,7 +7,7 @@ import java.io.Serializable
 open class Foodtruck(val name: String, val model: String, val latitude: Double, val longitude: Double, var menu: Menu? = null) : Serializable
 
 //when we use a GET request to see the food truck from the backend, we will get all the reviews other diners will have left it.
-class SaveableFoodtruck(val allReviews: MutableList<Reviews>, name: String, model: String, latitude: Double, longitude: Double, menu: Menu? = null) : Foodtruck(name, model, latitude, longitude, menu)
+class SaveableFoodtruck(val isFavorited: Boolean, val allReviews: MutableList<Reviews>, name: String, model: String, latitude: Double, longitude: Double, menu: Menu? = null) : Foodtruck(name, model, latitude, longitude, menu)
 
 //this is a list of reviews that we are getting back when we query the foodtruck
 class Reviews(val score: Double, val description: String, val userName: String)
